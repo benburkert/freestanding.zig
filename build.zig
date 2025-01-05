1effcf8e69234c3f7304644357024997448c1778
Ben Burkert <ben@benburkert.com>
2025-01-05 14:01:38 -0500

    fix build.zig, rename root.zig to freestanding.zig

--- build.zig
+++ build.zig
@@ -5,25 +5,9 @@ pub fn build(b: *std.Build) void {
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/root.zig"),
+    _ = b.addModule("freestanding", .{
+        .root_source_file = b.path("src/freestanding.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    const lib = b.addStaticLibrary(.{
-        .name = "freestanding.zig",
-        .root_module = lib_mod,
-    });
-
-    b.installArtifact(lib);
-
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
 }
